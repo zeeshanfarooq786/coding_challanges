27a2bbd104601fe52dc1df8ae29dd88666900235
zeeshanfarooq123@gmail.com <zeeshanfarooq123@gmail.com>
2024-02-02 12:18:56 +0500

    add_title

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,7 +4,7 @@
     <head>
 
         <meta charset="utf-8" />
-        <title>Oxford Business College</title>
+        <title>Connect Groups</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
         <meta content="Themesbrand" name="author" />
